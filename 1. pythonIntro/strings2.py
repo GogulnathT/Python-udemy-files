85375b3b635effc66900cf4a0fe26c0d3ad6c037
Gogul <gogul.lucky@gmail.com>
2023-02-17 15:54:28 +0530

    Python learning 17-2-23

---
+++ 1. pythonIntro/strings2.py
@@ -0,0 +1,15 @@
+from tkinter import N
+
+
+text = 'Ironman is a marvel superhero'
+
+print(text)
+# print(text[4])
+# print(text[5:11])
+# print(text[-4])
+
+name = 'Gogulnath'
+print(name[1:6:2])
+print(name[1:6])
+print(name[:6])
+print(name[3:])
