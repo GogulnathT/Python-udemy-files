523d91a55998f276e010d3c17abc19b5b92a50c3
Gogul <gogul.lucky@gmail.com>
2023-02-22 18:28:04 +0530

    Python learning 22-2-23

---
+++ 2-program-flow-control/blocks.py
@@ -0,0 +1,7 @@
+for i in range(1, 14):
+    print('The number is {}'.format(i))
+print(i * 13)
+
+# here the print statement is in a block. blocks are marked by indentation(tab spaces). here there is
+# only one statement in the block and usually codes having multiple statements in one block
+# print in line3 is outside the block as it is not indented and does not get included in the loop
