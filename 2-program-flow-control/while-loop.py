2b97a8822a2520b05ecaecc647ac4167af135101
Gogul <gogul.lucky@gmail.com>
2023-03-13 18:33:09 +0530

    Python learning 13-3-23

--- 2-program-flow-control/while-loop.py
+++ 2-program-flow-control/while-loop.py
@@ -8,5 +8,10 @@ directions = ['north', 'south', 'east', 'west']
 choice = ''
 
 while choice not in directions:  # this condition will work as long as the choice is something not in the list
-    choice = input('enter the direction : ')
+    choice = input('enter the direction(or quit to exit the game) : ')
+    if choice.casefold() == 'quit': #here instead of casefold(), lower() can also be used
+        print('You have ended the game. Thank you')
+        break #using break in while loop
+
+
 print('you have chosen a valid direction')
