955941bac0f3c327c39b0b1fd100dee7c396efcc
Gogul <gogul.lucky@gmail.com>
2023-04-05 18:37:48 +0530

    Python learning 5-4-23

---
+++ 3-lists-and-tuples/appending-to-list.py
@@ -0,0 +1,27 @@
+random = ["randomness"]
+choice = ''
+choice = input('Do you want to append to the list?(Y/N)')
+while choice.casefold() != 'n':
+    value = input("Enter the value to be appended : ")
+    random.append(value)  # append method used to append to the list
+    choice = input('Do you want to append to the list?(Y/N)')
+
+print('The list random contains : ')
+print(random)  # this prints the whole list
+
+# iterating over the list with for loop is also possible
+print('obtaining index using index method')
+for item in random:
+    print("{0} - {1}".format(random.index(item), item))
+    # here .index() is a list method which returns the index of the item
+    # in the list; however it may not be the most efficient way to look up
+    # the index of an item in the list incase of large lists
+
+#index of valus in lists can also be obtained using the enumerate function;
+#it is a built in function which returns an enumerate object when a list type
+#is passed, which contains the value and index
+print('obtaining index using the enumrate function')
+for number,item in enumerate(random) : 
+    print('{0} : {1}'.format(number, item))
+#here in the for loop we are using 2 variables, if the object we are iterating 
+#over contains more than one value, 2 or more variables can be used
---
+++ 3-lists-and-tuples/enumerate-example.py
@@ -0,0 +1,7 @@
+#for enumerate def refer appending-to-list.py
+
+#enumerate can be used for any type of sequences eg. string,list etc
+#usually in enumerate the first variable denotes the index and the 
+#second variable denotes the value
+for index, character in enumerate('abcdefghijklmnopqrstuvwxyz'):
+    print(character, index)
